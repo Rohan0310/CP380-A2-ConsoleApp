8c8dbdfc77b90ed034b993a4d6b744c40c498a3c
Rohan0310 <rpalkar@confederationcollege.ca>
2021-08-08 16:29:19 -0400

    Completed the TODO list in program.cs

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -18,15 +18,28 @@ namespace BreadmakerReport
             var BMList = BreadmakerDb.Breadmakers
                 // TODO: add LINQ logic ...
                 //       ...
-                .ToList();
 
+                .Select(c => new
+                {
+                    Reviews = c.Reviews.Count,
+                    Average = (Double)c.Reviews.Average(a => a.stars),
+                    Adjust = ratingAdjustmentService.Adjust((Double)c.Reviews.Average(a => a.stars), (Double)c.Reviews.Count),
+                    Desc = c.title
+                })
+
+                .ToList()
+            .OrderByDescending(ratingAdjustmentService => ratingAdjustmentService.Adjust)
+            .ToList();
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
             {
                 var i = BMList[j];
+
+                Console.WriteLine($"[{j + 1}] {i.Reviews} {Math.Round(i.Average, 2)} {Math.Round(i.Adjust, 2)} {i.Desc}");
                 // TODO: add output
                 // Console.WriteLine( ... );
             }
+            Console.ReadLine();
         }
     }
 }
